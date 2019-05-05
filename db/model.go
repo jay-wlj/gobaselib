4bee0d5d50d5ac1514a2e9170151721801dadf5c
jay <305898636@qq.com>
2019-05-05 16:33:27 +0800

    添加psql model基础操作

---
+++ db/model.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+// 公共model更新时间及创建时间
+type Model struct {
+	CreateTime int64 `json:"create_time"`
+	UpdateTime int64 `json:"update_time"`
+}
+
+func (t *Model) BeforeCreate(scop *gorm.Scope) error {
+	fmt.Println("BeforeCreate")
+	scop.SetColumn("create_time", time.Now().Unix())
+	scop.SetColumn("update_time", time.Now().Unix())
+	return nil
+}
+
+// func (t *Model) BeforeSave(scop *gorm.Scope) error {
+// 	fmt.Println("BeforeSave")
+// 	scop.SetColumn("update_time", time.Now().Unix())
+// 	return nil
+// }
+
+func (t *Model) BeforeUpdate(scop *gorm.Scope) error {
+	fmt.Println("BeforeUpdate")
+	scop.SetColumn("update_time", time.Now().Unix())
+	return nil
+}
