0612198aeb6e532aa9eec95705f066065039a428
jayden <jayden@jaydendeMacBook-Pro.local>
2021-01-27 11:42:29 +0800

    add consulregcounter

--- hll/metrics/init.go
+++ hll/metrics/init.go
@@ -23,11 +23,6 @@ var (
 		promhttp.Handler().ServeHTTP(c.Writer, c.Request)
 	}
 
-	TestReqGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "hllci_api_request_test_gauge",
-		Help: "the total number of processed events",
-	}, []string{"hll_data_type", "hll_metric_type", "hll_appid", "hll_env", "hll_ip"})
-
 	reqLatencyHisgoram = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name: "hllci_api_request_seconds",
 		Help: "the total number of processed events",
@@ -57,4 +52,9 @@ var (
 		Name: "hllci_downstream_http_request_seconds",
 		Help: "the total number of redis processed events",
 	}, []string{"hll_data_type", "error", "downstream_url", "hll_metric_type", "hll_appid", "hll_env", "hll_ip"})
+
+	ConsulRegCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "hllci_consul_reg_count",
+		Help: "the total number of reg in the consul",
+	}, []string{"reg_name", "reg_id", "error", "hll_data_type", "hll_metric_type", "hll_appid", "hll_env", "hll_ip"})
 )
